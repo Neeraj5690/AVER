ffd0704e0e6ac2d7360b49f28342a27b22d945e9
neeraj <neeraj.kumar@bitsinglass.com>
2022-03-08 15:31:17 +0530

    General push

--- AverTest/test_InvoiceEntry/EmailReportContent/SR_SmokeAll_together.py
+++ AverTest/test_InvoiceEntry/EmailReportContent/SR_SmokeAll_together.py
@@ -79,9 +79,9 @@ def test_ReportSendSmokeAll():
 
 
     email_from = 'Rae CRM Test Automation Team'
-    email_to =['neeraj1wayitsol@gmail.com','avneet.kumar@bitsinglass.com','sumreet.kaur@bitsinglass.com','gagandeep.singh@bitsinglass.com','reilly@averplanmanagers.com.au','scott@scnsa.com.au']
+    #email_to =['neeraj1wayitsol@gmail.com','avneet.kumar@bitsinglass.com','sumreet.kaur@bitsinglass.com','gagandeep.singh@bitsinglass.com','reilly@averplanmanagers.com.au','scott@scnsa.com.au']
     #email_to = ['neeraj1wayitsol@gmail.com', 'avneet.kumar@bitsinglass.com', 'sumreet.kaur@bitsinglass.com', 'gagandeep.singh@bitsinglass.com']
-    #email_to =['gagandeep.singh@bitsinglass.com','neeraj1wayitsol@gmail.com']
+    email_to =['gagandeep.singh@bitsinglass.com','neeraj1wayitsol@gmail.com']
 
     SenderEmail = "Raeautomationbig@gmail.com"
     RandmStr = "Raeautomationbig@786"

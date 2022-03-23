6c4d68c0314bb9b9d7a36cca91973088c9ea133d
neeraj <neeraj.kumar@bitsinglass.com>
2022-03-23 19:33:59 +0530

    General push

--- AverTest/test_InvoiceEntry/test_InvoiceListingPagination/test_InvoiceListingPagination.py
+++ AverTest/test_InvoiceEntry/test_InvoiceListingPagination/test_InvoiceListingPagination.py
@@ -302,7 +302,7 @@ def test_VerifyAllClickables(test_setup):
             totalClicks=driver.find_element_by_xpath("//tbody[@id='invoiceEntryListingAjaxView']/tr[last()]/td/nav/ul/li[last()]/preceding-sibling::li[1]/a").text
             totalClicks=int(totalClicks)
             print(totalClicks)
-
+            totalClicks = 1
             TotalPageAmount=0
             for l0 in range(totalClicks):
                 print()
@@ -314,17 +314,21 @@ def test_VerifyAllClickables(test_setup):
 
                 for l1 in range(TotalRecords+1):
                     if (l1 % 2) != 0:
-                        try:
-                            TotalAmountFound=driver.find_element_by_xpath("//tbody[@id='invoiceEntryListingAjaxView']/tr["+str(l1)+"]/td[10]").text
-                        except Exception:
-                            TotalAmountFound = driver.find_element_by_xpath(
-                                "//tbody[@id='invoiceEntryListingAjaxView']/tr["+str(l1+1)+"]/td[10]").text
-                        #print(TotalAmountFound)
-                        TotalAmountFound = re.sub('[^A-Za-z0-9.]+', '', TotalAmountFound)
-                        TotalAmountFoundFloat=float(TotalAmountFound)
-                        #print(TotalAmountFoundFloat)
-                        TotalAmountFloatList.append(TotalAmountFoundFloat)
-                        TotalPageAmount=TotalPageAmount+TotalAmountFoundFloat
+                        InvoiceStatus = driver.find_element_by_xpath(
+                            "//tbody[@id='invoiceEntryListingAjaxView']/tr[" + str(l1) + "]/td[11]").text
+                        print(InvoiceStatus)
+                        if InvoiceStatus != "Cancelled" or InvoiceStatus != "Declined":
+                            try:
+                                TotalAmountFound=driver.find_element_by_xpath("//tbody[@id='invoiceEntryListingAjaxView']/tr["+str(l1)+"]/td[10]").text
+                            except Exception:
+                                TotalAmountFound = driver.find_element_by_xpath(
+                                    "//tbody[@id='invoiceEntryListingAjaxView']/tr["+str(l1+1)+"]/td[10]").text
+                            #print(TotalAmountFound)
+                            TotalAmountFound = re.sub('[^A-Za-z0-9.]+', '', TotalAmountFound)
+                            TotalAmountFoundFloat=float(TotalAmountFound)
+                            #print(TotalAmountFoundFloat)
+                            TotalAmountFloatList.append(TotalAmountFoundFloat)
+                            TotalPageAmount=TotalPageAmount+TotalAmountFoundFloat
 
                 TestResult.append("On page no.["+str(l0+1)+"] total amount found is $"+str(round(TotalPageAmount,2)))
                 TestResultStatus.append("Pass")
